a4d77e3d14368067f3f0474c442e5d29557e1a5c
unknown <renetdevp@gmail.com>
2025-02-17 16:35:31 +0900

    250217_01_authentication

--- routes/authentication.js
+++ routes/authentication.js
@@ -13,12 +13,12 @@ router.post('/', async (req, res, next) => {
 
     try {
         const result = await readOne({ userId, hash });
-        let code = -1, msg = '';
 
         if (result === -1) throw new Error(`Error while read User ${userId}`);
         else if (result === 0){
-            code = 404;
-            msg = `User ${userId} not exists`;
+            return res.status(404).json({
+                msg: `User ${userId} not exists`
+            });
         }else {
             //  make jwt_token use result.id, jwtSecret and expiredTime.
             const jwtSecret = process.env.jwtSecret || 'thisissecret';
@@ -26,19 +26,16 @@ router.post('/', async (req, res, next) => {
 
             };
             //  https://stackoverflow.com/a/56872864, jwt.sign()은 callback 함수가 제공되면 비동기로, 제공되지 않으면 동기식으로 작동함
-            jwt.sign({ userId: result.id }, jwtSecret, jwtOption, (err, token) => {
+            jwt.sign({ userId: result.userId }, jwtSecret, jwtOption, (err, token) => {
                 if (err){
                     return next({ msg: `Failed to sign JWT`});
                 }
-
-                code = 201;
-                msg = token;
+                
+                res.status(201).json({
+                    msg: token
+                });
             });
         }
-
-        res.status(code).json({
-            msg
-        });
     }catch (e){
         next({
             msg: `Failed to authenticate User ${userId}`
